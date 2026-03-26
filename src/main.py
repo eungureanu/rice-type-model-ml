ddfc28c440436d1c7ae4f0dc8e62bc486678fc15
eungureanu <64928001+eungureanu@users.noreply.github.com>
2026-03-26 22:55:10 +0200

    Add methods for missing values, outliers and X,y split

--- src/main.py
+++ src/main.py
@@ -5,4 +5,7 @@ data_loader = DataChecker.DatasetLoader("/Users/Emma/Uni/An III/ML/Rice_data_typ
 df = data_loader.load_data()
 
 data_reader = DataChecker.DatasetReader(df)
-info = data_reader.data_summary()
+overview = data_reader.overview()
+valori_lipsa = data_reader.missing_values()
+outliers = data_reader.outliers()
+X, y = data_reader.split_features_target()
